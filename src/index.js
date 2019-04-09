3aecdd02564056adf15205148678e0a9e8b509fd
Pride <serafim0604@gmail.com>
2019-04-09 19:49:09 +0700

    added jQuery

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import 'jquery'
+import  'popper.js'
+
 import './sass/main.sass'
 import './js/button'
+
--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -2,6 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
 
 const PATHS = {
     src: path.join(__dirname, '../src'),
@@ -81,6 +82,11 @@ module.exports = {
         }),
         new CopyWebpackPlugin([
             { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
-        ])
+        ]),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        })
     ]
 };
