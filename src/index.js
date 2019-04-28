534096be96beee3aaf70900bcc89fe07368d17ab
Pride <serafim0604@gmail.com>
2019-04-28 11:28:31 +0700

    finished percentages/pie-chart

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import  'popper.js'
 
 import './sass/main.sass'
 import './js/standard-buttons/button'
-import './js/pie-chart/__donut'
+import './js/pie-chart/__doughnut'
 import './js/pie-chart/__percentages'
--- src/js/pie-chart/__percentages.js
+++ src/js/pie-chart/__percentages.js
@@ -2,11 +2,9 @@ var dataset = [0,38,62,89];
 
 function Draw(dataset)
 {
-
-    var canvas = document.getElementsByClassName('.pie-chart__percentages');
-
+    var canvas = document.getElementsByClassName('pie-chart__percentages');
     for (var k = 0; k < canvas.length; k++){
-        //canvas[k].style.height = '500px';
+
         if (canvas[k].getContext){
             var ctx = canvas[k].getContext('2d');
             ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -14,24 +12,26 @@ function Draw(dataset)
 
             var i = 1;
             var j = 1;
-            ctx.lineWidth = 10;
+            ctx.lineWidth = 8;
             ctx.strokeStyle = '#e75735';
-            ctx.fillStyle = '#9d9d9d';
+            ctx.fillStyle = '#b8b8b8';
 
-            ctx.beginPath();
-            var x = 25+j*50; // x coordinate
-            var y = 25+i*50; // y coordinate
-            var radius = 55; // Arc radius
-            var startAngle = -1.57; // Starting point on circle
-            var endAngle = -1.56+offset ; //Math.PI+(Math.PI*j)/2; // End point on circle
-            var anticlockwise =  false;//(i%2==0) ? false : true; // clockwise or anticlockwise
+            if (dataset[k] > 0) {
+                ctx.beginPath();
+                var x = 25 + j * 50; // x coordinate
+                var y = 25 + i * 50; // y coordinate
+                var radius = 65; // Arc radius
+                var startAngle = -1.57; // Starting point on circle
+                var endAngle = -1.56 + offset; //Math.PI+(Math.PI*j)/2; // End point on circle
+                var anticlockwise = false;//(i%2==0) ? false : true; // clockwise or anticlockwise
 
-            ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
-            ctx.stroke();
-
+                ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+                ctx.stroke();
+            }
             //-----------------------TXT---------------
-            ctx.font = 'bold 30px sans-serif';
-            ctx.fillText(dataset[k] + "%", 38, 80);
+            ctx.font = '4.8rem sans-serif';
+            ctx.fontWeight = 100;
+            ctx.fillText(dataset[k], 32, 100);
         }
     }
 }
