77135cb963239fc9bc67d608f9f8891a046fa5a7
Pride <serafim0604@gmail.com>
2019-05-06 17:45:23 +0700

    finished 1st slider

--- src/js/sliders/__pointer.js
+++ src/js/sliders/__pointer.js
@@ -1,9 +1,21 @@
-var slider = document.getElementById('slider_valued');
-var value = document.getElementById('pointer');
+const slider = document.getElementById('slider_valued');
+const pointer = document.getElementById('pointer');
+pointer.innerHTML = 0;
 
+function setBagePosition(Slider, Pointer){
 
-value.innerHTML = slider.value;
-slider.oninput = function(){
-    value.innerHTML = this.value;
-    value.style.left = value.left + this.value + 'px';
+    var fontSize = window.getComputedStyle(document.body, null).getPropertyValue('font-size');
+
+    const diameter = parseInt(fontSize)*2.1; // approximated width of pointer.
+
+    const dxPixels = diameter/2 + (Slider.valueAsNumber-parseInt(Slider.min))*(Slider.scrollWidth-diameter)/(parseInt(Slider.max)-parseInt(Slider.min));
+
+    Pointer.style.left = dxPixels - (Pointer.offsetWidth / 2) + 'px';
+}
+
+setBagePosition(slider, pointer);
+
+slider.oninput = function() {
+    pointer.innerHTML = slider.value;
+    setBagePosition(slider, pointer);
 }
