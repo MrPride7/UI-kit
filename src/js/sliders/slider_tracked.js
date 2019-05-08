08e4ac07093a9e787b374583493129b9347e2bea
Pride <serafim0604@gmail.com>
2019-05-08 20:31:19 +0700

    sliders finished

---
+++ src/js/sliders/slider_tracked.js
@@ -0,0 +1,11 @@
+const sliderTrk = document.getElementById('slider_tracked');
+
+console.log('Test1 =' + sliderTrk.value);
+
+sliderTrk.addEventListener("mousemove", function () {
+    var x = sliderTrk.value;
+    console.log('Test2 =' + sliderTrk.value);
+    var color = 'linear-gradient(90deg, #4eb7a8 ' + x + '%, #e5e5e5 ' + x + '%)';
+    sliderTrk.style.background = color;
+})
+
--- src/js/sliders/slider_valued.js
+++ src/js/sliders/slider_valued.js
@@ -1,14 +1,14 @@
 const slider = document.getElementById('slider_valued');
 const pointer = document.getElementById('pointer');
-pointer.innerHTML = 0;
+pointer.innerHTML = slider.value;
 
 function setBagePosition(Slider, Pointer){
 
     var fontSize = window.getComputedStyle(document.body, null).getPropertyValue('font-size');
 
     const diameter = parseInt(fontSize)*2.1; // approximated width of pointer.
-
-    const dxPixels = diameter/2 + (Slider.valueAsNumber-parseInt(Slider.min))*(Slider.scrollWidth-diameter)/(parseInt(Slider.max)-parseInt(Slider.min));
+    const dxPixels = diameter/2 + (Slider.valueAsNumber-parseInt(Slider.min))*
+        (Slider.scrollWidth-diameter)/(parseInt(Slider.max)-parseInt(Slider.min));
 
     Pointer.style.left = dxPixels - (Pointer.offsetWidth / 2) + 'px';
 }
@@ -16,6 +16,7 @@ function setBagePosition(Slider, Pointer){
 setBagePosition(slider, pointer);
 
 slider.oninput = function() {
-    pointer.innerHTML = slider.value;
+    pointer.innerHTML = this.value;
     setBagePosition(slider, pointer);
 }
+
