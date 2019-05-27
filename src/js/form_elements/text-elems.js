0af1af0f27ee9b8cde9bda383f2680baafadd060
Pride <serafim0604@gmail.com>
2019-05-27 18:08:59 +0700

    form validation 3/4 finished

--- src/js/form_elements/text-elems.js
+++ src/js/form_elements/text-elems.js
@@ -1,8 +1,7 @@
-
- var forms = document.getElementsByTagName('form');
-for(var i = 0; i < forms.length; i++){
+var forms = document.getElementsByTagName('form');
+/*for(var i = 0; i < forms.length; i++){
     forms[i].addEventListener('submit', validator);
-}
+}*/
 
 var rules = {
     required: function(el){
@@ -16,15 +15,11 @@ var rules = {
         return reg.test(el.value);
     },
     name: function(el){
-        var reg = new RegExp("[A-Za-zА-Яа-яЁё]+[^@s]+[A-Za-zА-Яа-яЁё]");
+        var reg = new RegExp("^[А-ЯЁ][а-яё]*([-][А-ЯЁ][а-яё]*)?\\s[А-ЯЁ][а-яё]");
         return reg.test(el.value);
     }
 }
-
-/*function showErrors (arr) {
-    console.log(arr);
-}*/
-
+/*
 function validator (e) {
     //e.preventDefault();
     var errors =[];
@@ -32,7 +27,6 @@ function validator (e) {
     for(var i = 0; i < inputs.length; i++){
         if (inputs[i].tagName != 'BUTTON') {
             var rulesList = inputs[i].dataset.rule;
-            //console.log(rulesList);
             if (rulesList !== undefined){
                 rulesList = rulesList.split(' ');
                 for(var j = 0; j < rulesList.length; j++){
@@ -42,11 +36,6 @@ function validator (e) {
                                 name: inputs[i].name,
                                 error: rulesList[j]
                             });
-                            inputs[i].nextElementSibling.classList.add("form__indicator_invalid");
-                            inputs[i].nextElementSibling.innerHTML = "eror";
-                        } else {
-                            inputs[i].nextElementSibling.classList.add("form__indicator_valid");
-                            inputs[i].nextElementSibling.innerHTML = "Thanks!";
                         }
                     }
                 }
@@ -57,4 +46,46 @@ function validator (e) {
         e.preventDefault();
         //showErrors(errors);
     }
+}*/
+for (var k = 0; k < forms.length; k++){
+    var formElems = forms[k].elements;
+    formElems = Array.prototype.slice.call(formElems);
+    for (var t = 0; t < formElems.length; t++){
+         if (formElems[t].tagName !== "INPUT"){
+          formElems.splice(t);
+         }
+     }
 }
+formElems.forEach(function(formInput) {
+    formInput.onchange = function () {
+        var rulesList = formInput.dataset.rule;
+        if (rulesList !== undefined){
+            rulesList = rulesList.split(' ');
+            for(var i = 0; i < rulesList.length; i++){
+                if (rulesList[i] in rules) {
+                    if(!rules[rulesList[i]](formInput)){
+                        if(formInput.nextElementSibling.classList.contains('form__indicator_valid')){
+                            formInput.nextElementSibling.classList.remove('form__indicator_valid');
+                        }
+                        if(formInput.nextElementSibling.classList.contains('form__indicator_invalid')){
+                            formInput.nextElementSibling.classList.remove('form__indicator_invalid');
+                        }
+                        formInput.nextElementSibling.classList.add('form__indicator_invalid');
+                        formInput.nextElementSibling.innerHTML = "eror";
+                    }
+                    else {
+                        if(formInput.nextElementSibling.classList.contains('form__indicator_valid')){
+                            formInput.nextElementSibling.classList.remove('form__indicator_valid');
+                        }
+                        if(formInput.nextElementSibling.classList.contains('form__indicator_invalid')){
+                            formInput.nextElementSibling.classList.remove('form__indicator_invalid');
+                        }
+                        formInput.nextElementSibling.classList.add('form__indicator_valid');
+                        formInput.nextElementSibling.innerHTML = "Thanks!";
+                    }
+                }
+            }
+        }
+    }
+})
+//console.log(formElems);
